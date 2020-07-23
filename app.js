064b84bf3111449d2bb392f68fc3951cdd61783d
Mai <nmai601@gmail.com>
2020-07-23 20:40:40 +0000

    add mongodb atlas

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes = require("./routes/comments"),
 	indexRoutes = require("./routes/index");
 
 const mongoose	= require("mongoose");
-mongoose.connect('mongodb://localhost:27017/yelp_camp', {
+//mongoose.connect('mongodb://localhost:27017/yelp_camp'
+mongoose.connect('mongodb+srv://nmai601:password123456@cluster0.xan0n.mongodb.net/<dbname>?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
